3c7e39623ce433aa732ade6e362765beab603e7a
jyotish1136 <151714372+jyotish1136@users.noreply.github.com>
2025-03-27 16:26:42 +0530

    import update

--- src/main/java/com/jyotish/portfolio/configuration/WebConfig.java
+++ src/main/java/com/jyotish/portfolio/configuration/WebConfig.java
@@ -1,5 +1,5 @@
 package com.jyotish.portfolio.configuration;
-
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
