bb0f1dba9a7b57d5582f148b3cd229ec09150582
jyotish1136 <151714372+jyotish1136@users.noreply.github.com>
2025-03-27 16:16:37 +0530

    Update WebConfig

--- src/main/java/com/jyotish/portfolio/configuration/WebConfig.java
+++ src/main/java/com/jyotish/portfolio/configuration/WebConfig.java
@@ -5,10 +5,12 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+    @Value(${frontend.uri})
+    private String frontendURI;
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://jyotish-kumar.onrender.com/")
+                .allowedOrigins(frontendURI)
                 .allowedMethods("POST")
                 .allowedHeaders("*")
                 .allowCredentials(true);
