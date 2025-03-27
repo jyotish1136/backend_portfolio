5c02085166fcc20d00ab2daa1bc162d29fad71c2
jyotish1136 <151714372+jyotish1136@users.noreply.github.com>
2025-03-27 16:20:09 +0530

    uri config

--- src/main/java/com/jyotish/portfolio/configuration/WebConfig.java
+++ src/main/java/com/jyotish/portfolio/configuration/WebConfig.java
@@ -5,7 +5,7 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-    @Value(${frontend.uri})
+    @Value("${frontend.uri}")
     private String frontendURI;
     @Override
     public void addCorsMappings(CorsRegistry registry) {
